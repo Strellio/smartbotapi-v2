67b406fb9206f74d9ead669d8c5cf4287703c748
banphlet <annor_theophilus@hotmail.com>
2020-12-27 14:07:53 +0000

    updated

--- src/api/graphql/analytics/resolvers/queries.ts
+++ src/api/graphql/analytics/resolvers/queries.ts
@@ -23,7 +23,7 @@ export default {
       ])
 
       return {
-        total_users: usersCount,
+        total_customers: usersCount,
         total_messages: messagesCount,
         total_tickets: ticketsCount
       }
--- src/api/graphql/analytics/schema/types.ts
+++ src/api/graphql/analytics/schema/types.ts
@@ -4,7 +4,7 @@ import { gql } from 'apollo-server-express'
 
 export default gql`
   type Counts {
-    total_users: Int!
+    total_customers: Int!
     total_tickets: Int!
     total_messages: Int!
   }
