f3dc552beb567b6feeab36669146bbed7b92f912
theophilus annor <annor_theophilus@hotmail.com>
2023-08-27 15:35:12 +0000

    update with changes

--- src/api/middlewares/is-authenticated.ts
+++ src/api/middlewares/is-authenticated.ts
@@ -1,29 +1,27 @@
-'use strict'
-import { required, decodeJwt } from '../../lib/utils'
-import customError from '../../lib/errors/custom-error'
-import businessService from '../../services/businesses'
-import businesses from '../../models/businesses'
+"use strict";
+import { required, decodeJwt } from "../../lib/utils";
+import customError from "../../lib/errors/custom-error";
+import businessService from "../../services/businesses";
+import businesses from "../../models/businesses";
 
 const throwAuthError = () =>
   customError({
-    name: 'UnAuthenticatedError',
-    message: 'you are not authorized to access this resource'
-  })
+    name: "UnAuthenticatedError",
+    message: "you are not authorized to access this resource",
+  });
 
-export default async function isAuthenticated (
-  token: string = required('token'),
+export default async function isAuthenticated(
+  token: string = required("token"),
   req: object = {}
 ) {
   try {
-    const decoded: any = await decodeJwt(token)
-
+    const decoded: any = await decodeJwt(token);
     return {
       business: await businessService().getById(decoded.business_id),
-      ...req
-    }
-
+      ...req,
+    };
   } catch (error) {
-    console.log(error)
-    throw throwAuthError()
+    console.log(error);
+    throw throwAuthError();
   }
 }
