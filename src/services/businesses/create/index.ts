1553b6e372b1d96b367b2011e131ce868060fd82
wisdom0063 <kwartengwisdomug95@gmail.com>
2023-08-30 18:16:01 +0000

    updating shopify sync data service

--- src/services/businesses/create/index.ts
+++ src/services/businesses/create/index.ts
@@ -1,79 +1,89 @@
-'use strict'
-import BusinessModel from '../../../models/businesses'
-import schema from './schema'
-import { Business } from '../../../models/businesses/types'
-import { validate } from '../../../lib/utils'
-import errors from '../../../lib/errors'
-import userService from '../../users'
-import queues from '../../../lib/queues'
-import { createSearchIndex } from '../../../lib/db/atlas'
+"use strict";
+import BusinessModel from "../../../models/businesses";
+import schema from "./schema";
+import { Business } from "../../../models/businesses/types";
+import { validate } from "../../../lib/utils";
+import errors from "../../../lib/errors";
+import userService from "../../users";
+import queues from "../../../lib/queues";
+import { createSearchIndex } from "../../../lib/db/atlas";
+import logger from "../../../lib/logger";
 
 interface CreateBusinessParams {
-  domain: string
-  email: string
-  status: string
-  external_id: string
-  business_name: string
+  domain: string;
+  email: string;
+  status: string;
+  external_id: string;
+  business_name: string;
   shop: {
-    external_created_at?: string
-    external_updated_at?: string
-    external_platform_domain: string
-    external_access_token?: string
-    external_refresh_token?: string
-    money_format?: string
-    external_platform_secret?: string
-    external_platform_client?: string
-  }
-  full_name?: string
+    external_created_at?: string;
+    external_updated_at?: string;
+    external_platform_domain: string;
+    external_access_token?: string;
+    external_refresh_token?: string;
+    money_format?: string;
+    external_platform_secret?: string;
+    external_platform_client?: string;
+  };
+  full_name?: string;
   location?: {
-    country: string
-    city: string
-  }
+    country: string;
+    city: string;
+  };
 }
 
 const ensureBusinessDoesNotExist = async (externalPlatformDomain: string) => {
   const business = await BusinessModel().getByExternalPlatformDomain(
     externalPlatformDomain
-  )
+  );
   if (business) {
     throw errors.throwError({
       name: errors.ResourceAlreadyExists,
-      message: 'business already exists'
-    })
+      message: "business already exists",
+    });
   }
-}
+};
 
-export default async function create (
+export default async function create(
   params: CreateBusinessParams
 ): Promise<Business> {
-  validate(schema, params)
-  await ensureBusinessDoesNotExist(params.shop.external_platform_domain)
+  validate(schema, params);
+  await ensureBusinessDoesNotExist(params.shop.external_platform_domain);
   const user = await userService().updateOrCreate({
     email: params.email,
     full_name: params.full_name,
-    country: params.location?.country
-  })
+    country: params.location?.country,
+  });
 
   const business = await BusinessModel().create({
     data: {
       ...params,
       account_name: params.business_name.replace(" ", "-").toLocaleLowerCase(),
-      user: user.id
-    }
-  })
+      user: user.id,
+    },
+  });
 
+  const productSyncQueue = queues.productSyncQueue();
+  const orderSyncQueue = queues.orderSyncQueue();
 
-  const queue = queues.productSyncQueue()
-      
-  await queue.add({ data: { business }, jobId: business.id })
+  await Promise.all([
+    productSyncQueue.add({ data: { business }, jobId: business.id }),
+    orderSyncQueue.add({ data: { business }, jobId: business.id }),
+  ]);
 
-  console.log("queue added for ", business.business_name)
-  
-  const result = await createSearchIndex({ dbName: business.account_name, indexName: "products-retriever", collectionName: "products-store" })
-  
-  console.log(result)
-  
-
-
-  return business
+  logger().info("queue added for ", business.business_name);
+  await Promise.all([
+    createSearchIndex({
+      dbName: business.account_name,
+      indexName: "products-retriever",
+      collectionName: "products-store",
+    }),
+    createSearchIndex({
+      dbName: business.account_name,
+      indexName: "orders-retriever",
+      collectionName: "orders-store",
+    }),
+  ]);
+  logger().info("Done adding indexes for ", business.business_name);
+  return business;
 }
