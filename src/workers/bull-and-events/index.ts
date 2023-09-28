96de52d1c3664368641b18d470bd962c9df6b8fa
wisdom0063 <kwartengwisdomug95@gmail.com>
2023-09-28 23:12:58 +0000

    making bull worker http based

--- src/workers/bull-and-events/index.ts
+++ src/workers/bull-and-events/index.ts
@@ -6,14 +6,39 @@ import pubsub from "../../lib/pubsub";
 import config from "../../config";
 import handleEvent from "./events";
 import planService from "../../services/plans";
-import H from "highland"
+import H from "highland";
+import express from "express";
+import cors from "cors";
+import loggerMaker from "../../lib/logger";
+
+const app = express();
+
+function startServer() {
+  const PORT = config.PORT;
+
+  const reqLogger = require("express-pino-logger")({
+    logger: loggerMaker(),
+  });
+
+  app
+    .use(cors())
+    .use(express.json())
+    .use(express.urlencoded({ extended: false }))
+    .use(reqLogger)
+    .get("/health", (req, res) => res.sendStatus(200))
+    .listen(PORT, () => {
+      loggerMaker().info(`Server started on http://localhost:${PORT}`);
+    });
+}
 
 db.connect().then(() => {
   syncProductsWorker();
-    syncOrdersWorker();
-   H(planService().getAll()).collect().toPromise(Promise)
+  syncOrdersWorker();
+  H(planService().getAll()).collect().toPromise(Promise);
   pubsub.googlePubSub.subscribe({
     subscriberName: config.SHOPIFY_GOOGLE_PUB_SUB_SUBSCRIPTION_NAME,
     handler: handleEvent,
   });
+
+  startServer();
 });
