65a5e79791c79df61978c3a1cc12de3c4740d7eb
wisdom0063 <kwartengwisdomug95@gmail.com>
2023-12-04 03:40:08 +0000

    exclude deleted chat platforms from populate

--- src/models/businesses/index.ts
+++ src/models/businesses/index.ts
@@ -7,7 +7,14 @@ import { Business } from "./types";
 const Model = mongoose.model("businesses", schema);
 const BusinessBaseModel = BaseModel(Model);
 
-const POPULATE = [{path: "plan", model:"plans"}, {path: "chat_platforms", model:"chat_platforms"}] as any
+const POPULATE = [
+  { path: "plan", model: "plans" },
+  {
+    path: "chat_platforms",
+    model: "chat_platforms",
+    match: { is_deleted: { $ne: true } },
+  },
+] as any;
 
 const getByEmail = (email: string = required("email")): Promise<Business> =>
   BusinessBaseModel.ensureExists({
